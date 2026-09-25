ed3b4a06c4fbb7283e472decca379000a5c1e7e5
derlygomezvl <derlygomezvl@unicauca.edu.co>
2026-09-25 13:57:12 -0500

    Primer commit: subiendo archivos iniciales

---
+++ docs/scripts/processElementData.js
@@ -0,0 +1,108 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_DHs0YbiIEfGwudgdwIVVtA,_aAB3AbiIEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Modelar Dominio y vocabulario_69AD520D_e4903bc6_Activity.jpeg"
+contentPage.processPage.imageFiles["_DHs0YbiIEfGwudgdwIVVtA,_aAB3AbiIEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Modelar Dominio y vocabulario_69AD520D_e4903bc6_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_DHs0YbiIEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/capabilitypatterns/resources/patron_identificar_requisitos_DF5DFA65_df5dfa65_Activity.jpeg"
+contentPage.processPage.imageFiles["_DHs0YbiIEfGwudgdwIVVtA,_RALnIbiIEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Definir el alcance_1EFD5AE5_99e0449e_Activity.jpeg"
+contentPage.processPage.imageFiles["_DHs0YbiIEfGwudgdwIVVtA,_RALnIbiIEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Definir el alcance_1EFD5AE5_99e0449e_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_Q8TsYbiLEfGwudgdwIVVtA,_eif1QbiLEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Detallar Requisitos_C9C85B3E_c6336dbf_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_vR-qUbiLEfGwudgdwIVVtA,_yYl18biLEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Validar y Acordar_BE3788CD_a32214ec_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_ewA3YbiMEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/Inicio_19810B4C_793d1abc_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/disciplina_requisitos_up_DDDE151C_ddde151c_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_i7hOgbiMEfGwudgdwIVVtA,_qaj9gbiNEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/Iteracion de construccion_5760195A_df8ef6ed_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_i7hOgbiMEfGwudgdwIVVtA,_qaj9gbiNEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/deliveryprocesses/resources/Iteracion de construccion_5760195A_df8ef6ed_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_g9dmYbiMEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/Elaboracion_5D3B8003_bcf78f73_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_i7hOgbiMEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/Construccion_959F4CCF_f55b5c3f_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_ewA3YbiMEfGwudgdwIVVtA,_CuQnYbiNEfGwudgdwIVVtA,_RALnIbiIEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Definir el alcance_1EFD5AE5_5ae49521_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_ewA3YbiMEfGwudgdwIVVtA,_CuQnYbiNEfGwudgdwIVVtA,_RALnIbiIEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Definir el alcance_1EFD5AE5_5ae49521_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_k1Wd8biMEfGwudgdwIVVtA,_7ZwpkbiNEfGwudgdwIVVtA,_yYl18biLEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Validar y Acordar_BE3788CD_6298b454_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_k1Wd8biMEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/Transicion_BD8745DC_1d43554c_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_g9dmYbiMEfGwudgdwIVVtA,_S5v-kbiNEfGwudgdwIVVtA,_aOL1gbiNEfGwudgdwIVVtA,_eif1QbiLEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Detallar Requisitos_C9C85B3E_f5b9644b_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_g9dmYbiMEfGwudgdwIVVtA,_S5v-kbiNEfGwudgdwIVVtA,_cyC5YbiNEfGwudgdwIVVtA,_yYl18biLEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Validar y Acordar_BE3788CD_ea4b53d_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_k1Wd8biMEfGwudgdwIVVtA,_7ZwpkbiNEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/patron_validar_requisitos_89FF8693_666f4633_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_ewA3YbiMEfGwudgdwIVVtA,_CuQnYbiNEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/deliveryprocesses/resources/patron_identificar_requisitos_5CC9AED8_7a95e3e8_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_ewA3YbiMEfGwudgdwIVVtA,_CuQnYbiNEfGwudgdwIVVtA,_aAB3AbiIEfGwudgdwIVVtAActivity"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Modelar Dominio y vocabulario_69AD520D_a5948c49_Activity.jpeg"
+contentPage.processPage.imageFiles["_Yj89wbiMEfGwudgdwIVVtA,_ewA3YbiMEfGwudgdwIVVtA,_CuQnYbiNEfGwudgdwIVVtA,_aAB3AbiIEfGwudgdwIVVtAActivityDetail"]="./../../PluginRequisitosUP/capabilitypatterns/resources/Modelar Dominio y vocabulario_69AD520D_a5948c49_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_k1Wd8biMEfGwudgdwIVVtA"] = ["Transicion","PluginRequisitosUP/deliveryprocesses/Transicion_BD8745DC.html"];
+contentPage.processPage.elementUrls["_u0oHU7iIEfGwudgdwIVVtA"] = ["Solicitudes de los Stakeholders","PluginRequisitosUP/workproducts/solicitudes_stakeholders_7971400D.html"];
+contentPage.processPage.elementUrls["_yYl18biLEfGwudgdwIVVtA"] = ["Validar y Acordar","PluginRequisitosUP/capabilitypatterns/Validar y Acordar_BE3788CD.html"];
+contentPage.processPage.elementUrls["_0aXgcLiLEfGwudgdwIVVtA"] = ["Revisor de Requisitos","PluginRequisitosUP/roles/revisor_de_requisitos_AB0BBEC.html"];
+contentPage.processPage.elementUrls["_kDLksLiNEfGwudgdwIVVtA"] = ["Linea base de requisitos","PluginRequisitosUP/deliveryprocesses/Linea base de requisitos_117767F1.html"];
+contentPage.processPage.elementUrls["_kSAdI7iIEfGwudgdwIVVtA"] = ["Vision","PluginRequisitosUP/workproducts/vision_5EFC460E.html"];
+contentPage.processPage.elementUrls["_0arCdLiLEfGwudgdwIVVtA"] = ["Paquete de Requisitos","PluginRequisitosUP/workproducts/paquete_de_requisitos_93AAE595.html"];
+contentPage.processPage.elementUrls["_i7hOgbiMEfGwudgdwIVVtA"] = ["Construccion","PluginRequisitosUP/deliveryprocesses/Construccion_959F4CCF.html"];
+contentPage.processPage.elementUrls["_u0Y2xriIEfGwudgdwIVVtA"] = ["Modelo de Casos de Uso","PluginRequisitosUP/workproducts/modelo_de_casos_de_uso_B0A78BEC.html"];
+contentPage.processPage.elementUrls["_gdEEgLiLEfGwudgdwIVVtA"] = ["Especificador de Requisitos","PluginRequisitosUP/roles/especificador_de_requisitos_2DF968EA.html"];
+contentPage.processPage.elementUrls["_gdC2YLiLEfGwudgdwIVVtA"] = ["Detallar un Caso de Uso","PluginRequisitosUP/tasks/detallar_caso_de_uso_ED597616.html"];
+contentPage.processPage.elementUrls["_wOYvkLiIEfGwudgdwIVVtA"] = ["Capturar Vocabulario Comun","PluginRequisitosUP/tasks/capturar_vocabulario_1DA60DC9.html"];
+contentPage.processPage.elementUrls["_K9XW0LiNEfGwudgdwIVVtA"] = ["Vision y alcance aprobados","PluginRequisitosUP/deliveryprocesses/Vision y alcance aprobados_E45C2C9.html"];
+contentPage.processPage.elementUrls["_0aUdILiLEfGwudgdwIVVtA"] = ["Revisar Requisitos","PluginRequisitosUP/tasks/revisar_requisitos_51EC5D16.html"];
+contentPage.processPage.elementUrls["_u0XooLiIEfGwudgdwIVVtA"] = ["Identificar Actores y Casos de Uso","PluginRequisitosUP/tasks/identificar_actores_casos_de_uso_677C02C4.html"];
+contentPage.processPage.elementUrls["_0aXgdriLEfGwudgdwIVVtA"] = ["Especificacion de Caso de Uso","PluginRequisitosUP/workproducts/especificacion_de_caso_de_uso_70A4C453.html"];
+contentPage.processPage.elementUrls["_hKfLMriLEfGwudgdwIVVtA"] = ["Vision","PluginRequisitosUP/workproducts/vision_5EFC460E.html"];
+contentPage.processPage.elementUrls["_kR_PAriIEfGwudgdwIVVtA"] = [" Stakeholder (Parte Interesada)","PluginRequisitosUP/roles/stakeholder_121DE2E3.html"];
+contentPage.processPage.elementUrls["_2dJlobiNEfGwudgdwIVVtA"] = ["Revisor de Requisitos","PluginRequisitosUP/roles/revisor_de_requisitos_AB0BBEC.html"];
+contentPage.processPage.elementUrls["_hKdWALiLEfGwudgdwIVVtA"] = ["Especificar Requisitos Complementarios","PluginRequisitosUP/tasks/especificar_requisitos_complementarios_14DC5462.html"];
+contentPage.processPage.elementUrls["_uy8TBriNEfGwudgdwIVVtA"] = ["Glosario","PluginRequisitosUP/workproducts/glosario_773CAA2D.html"];
+contentPage.processPage.elementUrls["_hKfLMLiLEfGwudgdwIVVtA"] = ["Analista de Sistemas","PluginRequisitosUP/roles/analista_de_sistemas_F2F80CED.html"];
+contentPage.processPage.elementUrls["_uy8TCLiNEfGwudgdwIVVtA"] = ["Requisitos Complementarios","PluginRequisitosUP/workproducts/requisitos_complementarios_4B80136A.html"];
+contentPage.processPage.elementUrls["_ewA3YbiMEfGwudgdwIVVtA"] = ["Inicio","PluginRequisitosUP/deliveryprocesses/Inicio_19810B4C.html"];
+contentPage.processPage.elementUrls["_uy52wLiNEfGwudgdwIVVtA"] = ["Detallar un Caso de Uso","PluginRequisitosUP/tasks/detallar_caso_de_uso_ED597616.html"];
+contentPage.processPage.elementUrls["_DHs0YbiIEfGwudgdwIVVtA"] = ["Identificar y Delimitar Requisitos","PluginRequisitosUP/capabilitypatterns/patron_identificar_requisitos_DF5DFA65.html"];
+contentPage.processPage.elementUrls["_7ZwpkbiNEfGwudgdwIVVtA"] = ["Validar y Acordar Requisitos","PluginRequisitosUP/deliveryprocesses/patron_validar_requisitos_89FF8693.html"];
+contentPage.processPage.elementUrls["_cyC5YbiNEfGwudgdwIVVtA"] = ["Validar y Acordar Requisitos","PluginRequisitosUP/deliveryprocesses/patron_validar_requisitos_EA1B5B91.html"];
+contentPage.processPage.elementUrls["_Q8TsYbiLEfGwudgdwIVVtA"] = ["Detallar Requisitos","PluginRequisitosUP/capabilitypatterns/patron_detallar_requisitos_BF29FB2D.html"];
+contentPage.processPage.elementUrls["_0aXgfLiLEfGwudgdwIVVtA"] = ["Vision","PluginRequisitosUP/workproducts/vision_5EFC460E.html"];
+contentPage.processPage.elementUrls["_u0Y2xLiIEfGwudgdwIVVtA"] = ["Actor","PluginRequisitosUP/workproducts/actor_E13F57B8.html"];
+contentPage.processPage.elementUrls["_0aXgcriLEfGwudgdwIVVtA"] = ["Analista de Sistemas","PluginRequisitosUP/roles/analista_de_sistemas_F2F80CED.html"];
+contentPage.processPage.elementUrls["_0aXgeLiLEfGwudgdwIVVtA"] = ["Modelo de Casos de Uso","PluginRequisitosUP/workproducts/modelo_de_casos_de_uso_B0A78BEC.html"];
+contentPage.processPage.elementUrls["_2dJlp7iNEfGwudgdwIVVtA"] = ["Informe de Revisión de Requisitos","PluginRequisitosUP/workproducts/informe_de_revisin_de_requisitos_42F035C8.html"];
+contentPage.processPage.elementUrls["_gdEEhriLEfGwudgdwIVVtA"] = ["Glosario","PluginRequisitosUP/workproducts/glosario_773CAA2D.html"];
+contentPage.processPage.elementUrls["_0arCd7iLEfGwudgdwIVVtA"] = ["Requisitos Acordados","PluginRequisitosUP/workproducts/requisitos_acordados_E687BBFE.html"];
+contentPage.processPage.elementUrls["_uy8TCriNEfGwudgdwIVVtA"] = ["Especificacion de Caso de Uso","PluginRequisitosUP/workproducts/especificacion_de_caso_de_uso_70A4C453.html"];
+contentPage.processPage.elementUrls["_uy8TALiNEfGwudgdwIVVtA"] = ["Especificador de Requisitos","PluginRequisitosUP/roles/especificador_de_requisitos_2DF968EA.html"];
+contentPage.processPage.elementUrls["_0aXgfriLEfGwudgdwIVVtA"] = ["Glosario","PluginRequisitosUP/workproducts/glosario_773CAA2D.html"];
+contentPage.processPage.elementUrls["_2dJlo7iNEfGwudgdwIVVtA"] = ["Analista de Sistemas","PluginRequisitosUP/roles/analista_de_sistemas_F2F80CED.html"];
+contentPage.processPage.elementUrls["_g9dmYbiMEfGwudgdwIVVtA"] = ["Elaboracion","PluginRequisitosUP/deliveryprocesses/Elaboracion_5D3B8003.html"];
+contentPage.processPage.elementUrls["_uy8TAriNEfGwudgdwIVVtA"] = [" Stakeholder (Parte Interesada)","PluginRequisitosUP/roles/stakeholder_121DE2E3.html"];
+contentPage.processPage.elementUrls["_kSAdIbiIEfGwudgdwIVVtA"] = ["Glosario","PluginRequisitosUP/workproducts/glosario_773CAA2D.html"];
+contentPage.processPage.elementUrls["_0aXgeriLEfGwudgdwIVVtA"] = ["Requisitos Complementarios","PluginRequisitosUP/workproducts/requisitos_complementarios_4B80136A.html"];
+contentPage.processPage.elementUrls["_2dJlpbiNEfGwudgdwIVVtA"] = ["Vision","PluginRequisitosUP/workproducts/vision_5EFC460E.html"];
+contentPage.processPage.elementUrls["_aAB3AbiIEfGwudgdwIVVtA"] = ["Modelar Dominio y vocabulario","PluginRequisitosUP/capabilitypatterns/Modelar Dominio y vocabulario_69AD520D.html"];
+contentPage.processPage.elementUrls["_uy8TBLiNEfGwudgdwIVVtA"] = ["Modelo de Casos de Uso","PluginRequisitosUP/workproducts/modelo_de_casos_de_uso_B0A78BEC.html"];
+contentPage.processPage.elementUrls["_RALnIbiIEfGwudgdwIVVtA"] = ["Definir el alcance","PluginRequisitosUP/capabilitypatterns/Definir el alcance_1EFD5AE5.html"];
+contentPage.processPage.elementUrls["_CuQnYbiNEfGwudgdwIVVtA"] = ["Identificar y Delimitar Requisitos","PluginRequisitosUP/deliveryprocesses/patron_identificar_requisitos_5CC9AED8.html"];
+contentPage.processPage.elementUrls["_0aXgdLiLEfGwudgdwIVVtA"] = [" Stakeholder (Parte Interesada)","PluginRequisitosUP/roles/stakeholder_121DE2E3.html"];
+contentPage.processPage.elementUrls["_u0Y2wriIEfGwudgdwIVVtA"] = ["Glosario","PluginRequisitosUP/workproducts/glosario_773CAA2D.html"];
+contentPage.processPage.elementUrls["_gdEEiriLEfGwudgdwIVVtA"] = ["Especificacion de Caso de Uso","PluginRequisitosUP/workproducts/especificacion_de_caso_de_uso_70A4C453.html"];
+contentPage.processPage.elementUrls["_qaj9gbiNEfGwudgdwIVVtA"] = ["Iteracion de construccion","PluginRequisitosUP/deliveryprocesses/Iteracion de construccion_5760195A.html"];
+contentPage.processPage.elementUrls["_kR_PALiIEfGwudgdwIVVtA"] = ["Analista de Sistemas","PluginRequisitosUP/roles/analista_de_sistemas_F2F80CED.html"];
+contentPage.processPage.elementUrls["_1IUyoLiLEfGwudgdwIVVtA"] = ["Acordar y Empaquetar Requisitos","PluginRequisitosUP/tasks/acordar_empaquetar_requisitos_13E0EE98.html"];
+contentPage.processPage.elementUrls["_u0Y2wLiIEfGwudgdwIVVtA"] = ["Vision","PluginRequisitosUP/workproducts/vision_5EFC460E.html"];
+contentPage.processPage.elementUrls["_gdEEhLiLEfGwudgdwIVVtA"] = ["Modelo de Casos de Uso","PluginRequisitosUP/workproducts/modelo_de_casos_de_uso_B0A78BEC.html"];
+contentPage.processPage.elementUrls["_eif1QbiLEfGwudgdwIVVtA"] = ["Detallar Requisitos","PluginRequisitosUP/capabilitypatterns/Detallar Requisitos_C9C85B3E.html"];
+contentPage.processPage.elementUrls["_kR-n8LiIEfGwudgdwIVVtA"] = ["Desarrollar la Vision","PluginRequisitosUP/tasks/desarrollar_vision_DCC9B40.html"];
+contentPage.processPage.elementUrls["_aOL1gbiNEfGwudgdwIVVtA"] = ["Detallar Requisitos","PluginRequisitosUP/deliveryprocesses/patron_detallar_requisitos_79F8FF2E.html"];
+contentPage.processPage.elementUrls["_vR-qUbiLEfGwudgdwIVVtA"] = ["Validar y Acordar Requisitos","PluginRequisitosUP/capabilitypatterns/patron_validar_requisitos_80C2EECB.html"];
+contentPage.processPage.elementUrls["_Yj89wbiMEfGwudgdwIVVtA"] = ["Disciplina de Requisitos – UP","PluginRequisitosUP/deliveryprocesses/disciplina_requisitos_up_DDDE151C.html"];
+contentPage.processPage.elementUrls["_kR_PBLiIEfGwudgdwIVVtA"] = ["Solicitudes de los Stakeholders","PluginRequisitosUP/workproducts/solicitudes_stakeholders_7971400D.html"];
+contentPage.processPage.elementUrls["_S5v-kbiNEfGwudgdwIVVtA"] = ["Iteracion de Elaboracion","PluginRequisitosUP/deliveryprocesses/Iteracion de Elaboracion_B34B0870.html"];
+contentPage.processPage.elementUrls["_0aXggLiLEfGwudgdwIVVtA"] = ["Informe de Revisión de Requisitos","PluginRequisitosUP/workproducts/informe_de_revisin_de_requisitos_42F035C8.html"];
+contentPage.processPage.elementUrls["_u0XoobiIEfGwudgdwIVVtA"] = ["Analista de Sistemas","PluginRequisitosUP/roles/analista_de_sistemas_F2F80CED.html"];
+contentPage.processPage.elementUrls["_2dJloLiNEfGwudgdwIVVtA"] = ["Revisar Requisitos","PluginRequisitosUP/tasks/revisar_requisitos_51EC5D16.html"];
+contentPage.processPage.elementUrls["_u0Xoo7iIEfGwudgdwIVVtA"] = [" Stakeholder (Parte Interesada)","PluginRequisitosUP/roles/stakeholder_121DE2E3.html"];
+contentPage.processPage.elementUrls["_gdLZQriLEfGwudgdwIVVtA"] = ["Solicitudes de los Stakeholders","PluginRequisitosUP/workproducts/solicitudes_stakeholders_7971400D.html"];
+contentPage.processPage.elementUrls["_gdEEgriLEfGwudgdwIVVtA"] = [" Stakeholder (Parte Interesada)","PluginRequisitosUP/roles/stakeholder_121DE2E3.html"];
+contentPage.processPage.elementUrls["__zVR4LiNEfGwudgdwIVVtA"] = ["Requisitos entregados y aceptados","PluginRequisitosUP/deliveryprocesses/Requisitos entregados y aceptados_23E9FEF.html"];
+contentPage.processPage.elementUrls["_gdEEiLiLEfGwudgdwIVVtA"] = ["Requisitos Complementarios","PluginRequisitosUP/workproducts/requisitos_complementarios_4B80136A.html"];
